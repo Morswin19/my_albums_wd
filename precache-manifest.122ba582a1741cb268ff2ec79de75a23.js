07b89d4c402d77f9d3856cf3a6e3f27e25cbfc45
Morswin19 <piotr.kalman85@gmail.com>
2020-08-21 20:26:38 +0200

    Updates

---
+++ precache-manifest.122ba582a1741cb268ff2ec79de75a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a945cc08539767bcf0098e61bb28d2a9",
+    "url": "/my_albums_wd/index.html"
+  },
+  {
+    "revision": "b679571746e584637e02",
+    "url": "/my_albums_wd/static/css/main.d12e9b5e.chunk.css"
+  },
+  {
+    "revision": "9d6215d13019edf52af4",
+    "url": "/my_albums_wd/static/js/2.090d4d50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my_albums_wd/static/js/2.090d4d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b679571746e584637e02",
+    "url": "/my_albums_wd/static/js/main.43ff3e49.chunk.js"
+  },
+  {
+    "revision": "d6c623fd207d84852be2",
+    "url": "/my_albums_wd/static/js/runtime-main.05e1dec4.js"
+  }
+]);
