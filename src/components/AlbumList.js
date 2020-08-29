f8e9604ee19b2b548b92f721d7872cd6d4928f6c
Morswin19 <piotr.kalman85@gmail.com>
2020-08-29 17:17:23 +0200

    layout changes

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,6 +5,10 @@ import Album from './Album';
 import DecadeSlider from './DecadeSlider';
 import RandomSection from './RandomSection';
 
+import blob from '../img/Path 2.svg';
+import wave from '../img/Group 20.svg';
+import wave2 from '../img/Group 30.svg';
+
 import '../styles/AlbumList.sass';
 
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -42,21 +46,25 @@ class AlbumList extends Component {
     }
 
     handlePaginationClick = (e) => {
+        window.scrollTo(window.scrollX, 920);
         this.setState({
             albumsSite: parseInt(e.target.innerHTML)
         })
     }
 
     handlePaginationArrowClick = (a) => {
+
         if (this.state.albumsSite > 1 && a === (-1)) {
             this.setState({
                 albumsSite: this.state.albumsSite + a
             })
+            window.scrollTo(window.scrollX, 920);
         } else if (a === 1) {
             if (this.props.time === "all" && (this.state.albumsSite < this.props.albums.length / 25)) {
                 this.setState({
                     albumsSite: this.state.albumsSite + a
                 })
+                window.scrollTo(window.scrollX, 920);
             } else if (this.props.time !== "all" && this.props.time !== "today"
             ) {
                 let albums = this.props.albums.filter(album => album.year >= this.props.time && album.year < parseInt(this.props.time) + 10)
@@ -64,6 +72,7 @@ class AlbumList extends Component {
                     this.setState({
                         albumsSite: this.state.albumsSite + a
                     })
+                    window.scrollTo(window.scrollX, 920);
                 }
             }
         }
@@ -121,7 +130,7 @@ class AlbumList extends Component {
         for (let i = 1; i <= albumSiteListAmount; i++) {
             albumSiteList.push(i);
         }
-        albumSiteList = albumSiteList.map(item => <li key={item} onClick={this.handlePaginationClick}>{item}</li>);
+        albumSiteList = albumSiteList.map(item => <li key={item} className={item === albumsSite ? 'active' : ''} onClick={this.handlePaginationClick}>{item}</li>);
         return (
             <div>
                 {/* 
@@ -136,6 +145,10 @@ class AlbumList extends Component {
                 <DecadeSlider timeArray={this.timeLine} />
                 <div id="albumList" className='albumList'>
                     {album}
+                    <img src={blob} alt="" id="blob5" className="blob" />
+                    <img src={wave} alt="" id="albumListWave1" className="wave" />
+                    <img src={wave2} alt="" id="albumListWave2" className="wave" />
+                    <img src={wave2} alt="" id="albumListWave3" className="wave" />
                 </div>
                 <div id="albumSites">
                     <span onClick={() => this.handlePaginationArrowClick(-1)}>{'<'}</span>
